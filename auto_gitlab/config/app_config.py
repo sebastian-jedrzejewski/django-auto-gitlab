ebd9747cfd0e1e48f338c1e5484422eef66ef3cf
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2023-09-05 13:44:54 +0200

    Make AppConfig a singleton

--- auto_gitlab/config/app_config.py
+++ auto_gitlab/config/app_config.py
@@ -14,6 +14,13 @@ from config.constants import (
 
 
 class AppConfig:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
     def __init__(self, config_data: Dict[str, any]):
         connection_data = config_data.get("connection", {})
         labels_data = config_data.get("labels", {})
--- auto_gitlab/config/app_config_instance.py
+++ auto_gitlab/config/app_config_instance.py
@@ -1,7 +1,14 @@
 from config.app_config import AppConfig
 from config.parser import read_config_file
 
+_configs = {}
+_app_config = None
+
 
 def get_app_config() -> AppConfig:
-    configs = read_config_file(".gitlab-config.yml")
-    return AppConfig(configs)
+    global _configs
+    global _app_config
+    if not _configs:
+        _configs = read_config_file(".gitlab-config.yml")
+        _app_config = AppConfig(_configs)
+    return _app_config
